9e6deba3a719c6a52d2f48866ab13c678c5e926b
Vishwas Chaugule <chaugule1369vish@gmail.com>
2025-02-15 23:34:51 +0530

    initial code for linq methods

---
+++ LINQDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+
+Console.WriteLine("Hello, World!");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+Console.WriteLine(numbers.First());
+Console.WriteLine(numbers.Last());
+Console.WriteLine(string.Join(",", numbers.ToArray().Reverse().Select(n => n.ToString())));
+Console.WriteLine(numbers.Skip(2).Take(1).First());
+
+Console.WriteLine(numbers.SkipWhile(i => i <3).TakeWhile(i => i >= 3).Last());
+Console.WriteLine(numbers.Single());
